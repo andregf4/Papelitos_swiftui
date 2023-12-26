2adce9630e453d43d97fe4b402691f16f3904dd4
André Foratto <andre.foratto@eldorado.org.br>
2023-12-26 08:31:59 -0300

    Initial Commit

---
+++ JogoFimDeAno/JogoFimDeAnoApp.swift
@@ -0,0 +1,17 @@
+//
+//  JogoFimDeAnoApp.swift
+//  JogoFimDeAno
+//
+//  Created by Andre Gerez Foratto on 26/12/23.
+//
+
+import SwiftUI
+
+@main
+struct JogoFimDeAnoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
